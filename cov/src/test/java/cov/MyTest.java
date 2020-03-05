75cc3298508971a3fc2dfb3e521466457f5582c4
cdouze <covs.pro@gmail.com>
2020-03-05 14:39:41 -0500

    jenkins

---
+++ cov/src/test/java/cov/MyTest.java
@@ -0,0 +1,11 @@
+package cov;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class MyTest {
+	@Test
+	public void test() {
+		assertEquals("True is true", true, true);
+	}
+}
